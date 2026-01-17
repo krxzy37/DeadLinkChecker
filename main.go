2dd3eacd9ff25fcd7ede4944bdd5ba8f78930fe3
krxzy37 <pala44archuk@gmail.com>
2026-01-17 22:30:11 +0200

    Create /GET request.

--- main.go
+++ main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const serverPort = 8080
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Проверка, чтобы не логировать запрос иконки от браузера
+		if r.URL.Path != "/favicon.ico" {
+			fmt.Printf("Server: %s %s\n", r.Method, r.URL.Path)
+		}
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", serverPort),
+		Handler: mux,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				fmt.Printf("Error running http server: %s\n", err)
+			}
+		}
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+
+	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
+	res, err := http.Get(requestURL)
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+}
