cd3f74e43f6f02b7fc9b546416453f3039abe6aa
krxzy37 <pala44archuk@gmail.com>
2026-01-31 19:33:11 +0200

    first working version scraping programm

--- main.go
+++ main.go
@@ -1,49 +1,60 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
-	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const serverPort = 8080
+var count = 0
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Проверка, чтобы не логировать запрос иконки от браузера
-		if r.URL.Path != "/favicon.ico" {
-			fmt.Printf("Server: %s %s\n", r.Method, r.URL.Path)
-		}
-		fmt.Fprintf(w, "Hello World!")
-	})
-
-	server := http.Server{
-		Addr:    fmt.Sprintf(":%d", serverPort),
-		Handler: mux,
-	}
-
-	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			if !errors.Is(err, http.ErrServerClosed) {
-				fmt.Printf("Error running http server: %s\n", err)
-			}
-		}
-	}()
-
-	time.Sleep(100 * time.Millisecond)
-
-	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
-	res, err := http.Get(requestURL)
+func chek(err error) {
 	if err != nil {
-		fmt.Printf("error making http request: %s\n", err)
+		fmt.Println(err)
+		fmt.Println("ERRRRRROOOOOR")
+
 		os.Exit(1)
 	}
+}
 
-	fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
+func main() {
+	URL := "https://ezgif.com/"
+
+	base, err := url.Parse(URL)
+	chek(err)
+
+	resp, err := http.Get(URL)
+	chek(err)
+	defer func() {
+		err := resp.Body.Close()
+		chek(err)
+	}()
+
+	if resp.StatusCode > 400 {
+		fmt.Println("StatusCode:", resp.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	chek(err)
+
+	parse := doc.Find("a").Each(func(index int, item *goquery.Selection) {
+
+		link, exist := item.Attr("href")
+		if exist {
+			refURL, err := url.Parse(link)
+			if err != nil {
+				fmt.Printf("Ссылка %v кривая, скип", link)
+				return
+			}
+			fullLink := base.ResolveReference(refURL)
+			count++
+			fmt.Printf("Ссылка %v, ее индекс %v\n", fullLink, count)
+
+		}
+	})
+	//chek(err)
+	parse.Size()
 }
