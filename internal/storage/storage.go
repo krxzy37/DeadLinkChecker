6c362e9584d1ae415fc5baa75f0c4e3c33aa778e
Oleksandr krzyz <pala44archuk@gmail.com>
2026-06-24 17:59:54 +0300

    Create Save() method

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,21 +1,39 @@
 package storage
 
 import (
+	"DeadLinkChecker/internal/scrapper"
+	"context"
 	"database/sql"
+	"fmt"
 
 	_ "modernc.org/sqlite"
 )
 
-type DataBase struct {
-	DB *sql.DB
+type Storage struct {
+	db *sql.DB
 }
 
-func (db *DataBase) ConnectDB() (*sql.DB, error) {
+func New(path string) (*Storage, error) {
 
-	database, err := sql.Open("sqlite", "stringurl")
+	db, err := sql.Open("sqlite", path)
 	if err != nil {
-		panic("Connect db error")
+		panic("db open error")
+	}
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("can't connect db: %w", err)
 	}
 
-	return database, nil
+	return &Storage{db: db}, nil
+
+}
+
+func (s *Storage) Save(ctx context.Context, p *scrapper.Page) error {
+
+	q := `INSERT INTO table (url, isDead) VALUES (?, ?)`
+
+	if _, err := s.db.ExecContext(ctx, q, p.URL, p.IsDead); err != nil {
+		return fmt.Errorf("cant save page: %w", err)
+	}
+
+	return nil
 }
